a6c51827afbc9ca4528857b85e78a98124c70376
DonlyNababan12 <donlynababan21@sma.belajar.id>
2025-02-05 14:29:42 +0700

    Delete pajak.py

--- pajak.py
+++
@@ -1,6 +0,0 @@
-nilai_pembelian = float(input("Masukkan nilai pembelian: "))
-
-pajak = 0.11 * nilai_pembelian
-total_bayar = nilai_pembelian + pajak
-
-print(f"Total yang harus dibayar setelah pajak: {total_bayar}")
