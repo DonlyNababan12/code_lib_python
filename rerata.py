727c6b82b089fb88ebafb07fb3080ee52d273b32
DonlyNababan12 <donlynababan21@sma.belajar.id>
2025-02-05 14:30:06 +0700

    Delete rerata.py

--- rerata.py
+++
@@ -1,9 +0,0 @@
-angka_list = []
-
-for i in range(10):
-    angka = float(input(f"Masukkan angka ke-{i+1}: "))
-    angka_list.append(angka)
-
-rerata = sum(angka_list) / len(angka_list)
-
-print(f"Rata-rata dari angka yang dimasukkan adalah: {rerata}")
